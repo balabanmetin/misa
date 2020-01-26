c72e8a4df1942574906120660dedf09577e28379
Metin Balaban <balaban@tscc-login1.sdsc.edu>
2020-01-25 22:31:39 -0800

    measure_RF can handle partial, present, and alien constituents

--- scripts/measure_RF.sh
+++ scripts/measure_RF.sh
@@ -4,15 +4,25 @@
 # $2 things.txt
 # $3 true.tree
 
+tre=`paste -s -d ' ' $2 | xargs nw_prune $1` 
+
 s1=`head -n 1 $2`
 s2=`tail -n +2 $2`
 
-t1=`cat $1 | sed "s/mix_1/$s1/g" | sed "s/mix_2/$s2/g"`
-t2=`cat $1 | sed "s/mix_2/$s1/g" | sed "s/mix_1/$s2/g"`
+t1=`echo $tre | sed "s/mix_1/$s1/g" `
+t2=`echo $tre | sed "s/mix_2/$s2/g" `
+t3=`echo $tre | sed "s/mix_1/$s2/g" `
+t4=`echo $tre | sed "s/mix_2/$s1/g" `
 
 v1=`compareTrees.missingBranch $3 <(echo $t1) -simplify | cut -f2 -d' '`
 v2=`compareTrees.missingBranch $3 <(echo $t2) -simplify | cut -f2 -d' '`
+v3=`compareTrees.missingBranch $3 <(echo $t3) -simplify | cut -f2 -d' '`
+v4=`compareTrees.missingBranch $3 <(echo $t4) -simplify | cut -f2 -d' '`
 
-printf "%s\n%s\n" $v1 $v2 | sort -n | head -n 1
 
 
+if ((v1+v2 < v3+v4 )); then
+    printf "%s\n%s\n" $v1 $v2
+else
+    printf "%s\n%s\n" $v3 $v4
+fi
